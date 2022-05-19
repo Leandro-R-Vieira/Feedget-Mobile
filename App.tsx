c1b537ea99e79ce9b3a4c6f2f52cdee565d1033b
Leandro Vieira <leandrorvieira22@yahoo.com>
2022-05-19 18:44:36 -0300

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,40 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import Leandro from './src/screens/ActivityIndicator';
+import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import { theme } from './src/theme';
+import Widget from './src/components/Widget';
+//import React from 'react-native/node_modules/@types/react';
+
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium
+  });
+
+  if (!fontsLoaded) {
+    return <Leandro />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{
+      flex: 1,
+      backgroundColor: theme.colors.background
+    }}
+    >
+      
+      
+
+      <StatusBar 
+      style="light" 
+      backgroundColor="transparent"
+      translucent
+      />
+      <Widget />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
